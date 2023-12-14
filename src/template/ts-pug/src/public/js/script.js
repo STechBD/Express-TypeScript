362b7555948686fcbe8a6971e9fd0ed2de03f5ff
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2023-12-15 02:43:49 +0600

    Update

--- src/template/ts-pug/src/public/js/script.js
+++ src/template/ts-pug/src/public/js/script.js
@@ -1,1 +1,29 @@
 console.log('Welcome to Create-Express.js by S Technologies')
+
+$(document).ready(function () {
+	$('#projectButton').click(function () {
+		$(this).toggleClass('text-sky-500')
+		$('#projectButton svg').toggleClass('text-sky-500')
+		$('#project').toggle()
+	})
+
+	$('#menuButton, #menuCloseButton').click(function () {
+		$('#menu').toggle()
+	})
+
+	$('#mobileProjectButton').click(function () {
+		$(this).toggleClass('text-sky-500')
+		$('#mobileProject').toggle()
+	})
+
+	$('#copy').click(function () {
+		const text = `npx create-express.js@latest
+`
+		navigator.clipboard.writeText(text).then(() => {
+			$('#copyText').text('Copied!')
+			setTimeout(() => {
+				$('#copyText').text('Copy')
+			}, 1000)
+		}).catch(e => console.log(e))
+	})
+})
